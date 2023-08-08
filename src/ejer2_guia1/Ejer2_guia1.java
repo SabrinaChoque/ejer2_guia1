aa2ebe9e4304d807bcced9bc0e31b91f241c1da8
SabrinaChoque <sabrinaechoque@gmail.com>
2023-08-08 19:18:43 -0300

    Initial commit

---
+++ src/ejer2_guia1/Ejer2_guia1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer2_guia1;
+
+/**
+ *
+ * @author familia maida
+ */
+public class Ejer2_guia1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
