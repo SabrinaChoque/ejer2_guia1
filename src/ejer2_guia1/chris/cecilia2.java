9a81a012d79937ac946b21e4f972b31f81023ea7
Cecilia Olejarczyk <chechuolejarczyk@gmail.com>
2023-08-08 19:35:30 -0300

    Create cecilia2.java
    hice una clase mia en el proyecto de chris

---
+++ src/ejer2_guia1/chris/cecilia2.java
@@ -0,0 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer2_guia1.chris;
+
+public class cecilia2 {
+    private String name;
+}
