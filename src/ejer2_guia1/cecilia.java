0ae3a54d1650393a0bbac8ca52f58849643b0fa7
Cecilia Olejarczyk <chechuolejarczyk@gmail.com>
2023-08-08 19:33:54 -0300

    Create cecilia.java
    hola

---
+++ src/ejer2_guia1/cecilia.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer2_guia1;
+
+
+public class cecilia {
+    private String name;
+}
